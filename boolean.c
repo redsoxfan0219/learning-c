a5d661c8d938424c2fdb9c7258437640f29b5040
Benjamin Moran <benjamin.moran.287@gmail.com>
2024-06-04 11:42:41 -0400

    Add ifelse test

---
+++ boolean.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    bool value1 = true;
+    bool value2 = false;
+
+    printf("\nvalue1: %d\n", value1);
+    printf("\nvalue2: %d\n\n", value2);
+
+    // Logical Operators
+
+    int x = 4;
+    int y = 6;
+
+    printf("\nIs x greater than y?   %d\n", x>y);
+    printf("\nIs x less than y?   %d\n", x<y);
+    printf("\nIs x equal to y?   %d\n\n", x==y);
+
+}
