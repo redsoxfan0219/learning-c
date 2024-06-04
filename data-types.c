09a6aea85ed00d5e1cc07de0d65790f6956b82d3
Benjamin Moran <benjamin.moran.287@gmail.com>
2024-06-03 22:43:26 -0400

    Finish data types

--- data-types.c
+++ data-types.c
@@ -2,5 +2,23 @@
 
 int main() {
 
-    
+/*
+
+int (4 bytes) | %d for printing
+double (8 bytes) | %lf for printing
+float (4 bytes) | %f for printing
+char (1 byte) | %c for printing
+
+*/
+
+    int age = 25;
+    double bigno = 250000000000000.19;
+    float decimal = 25.54f;
+    char dog = 'd';
+
+    printf("\nAge: %d", age);
+    printf("\nBig Number: %.2lf", bigno);
+    printf("\nDecimal: %f", decimal);
+    printf("\nCharacter: %c\n\n", dog); 
+
 }
